8e8b03285f3d225f596d76d205ee578c14547bc1
erdiha <erdiha@hotmail.com>
2024-04-13 17:34:55 -0700

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div className="bg-black flex w-screen h-screen">
+    <RouterView />
+  </div>
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<!-- <style lang="scss" scoped>
+/* Your SCSS styles go here */
+
+</style> -->
